7ba94d080f7ad9cd18114688c271244777588109
Guy AKROMAN <961theking@gmail.com>
2019-08-08 10:29:46 +0200

    add hive-env.sh file

---
+++ playbook/roles/role_hadoop/files/hive-env.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+if [ "SERVICE" = "cli"] ; then
+  if [ -z "$DEBUG" ]; then
+    export HADOOP_OPTS="$HADOOP_OPTS -XX:NewRatio=12 -Xms10m -XX:MaxHeapFreeRatio=40 -XX:MinHeapFreeRatio=15 -XX:+UseParNewGC -XX:-UseGCOverheadLimit"
+  else
+    export HADOOP_OPTS="$HADOOP_OPTS -XX:NewRatio=12 -Xms10m -XX:MaxHeapFreeRatio=40 -XX:MinHeapFreeRatio=15 -XX:-UseGCOverheadLimit"
+  fi
+fi
